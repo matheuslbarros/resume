fcf80d533d6561e38316f75f60995fb9b2000fcd
Matheus Lisboa de Barros <matheuslbarros@gmail.com>
2017-11-17 11:21:02 -0200

    close #20 Fix update experience

--- src/controllers/profile/experience/update.js
+++ src/controllers/profile/experience/update.js
@@ -1,7 +1,12 @@
 import Profile from '../../../models/profile';
 
 export default (req, res) => {
-  Profile.findOneAndUpdate({ user: req.user.id, "experience._id": req.params.id }, { '$set': { 'experience.$': req.body } })
+  var body = {};
+  for(var key in req.body) {
+    body["experience.$." + key] = req.body[key];
+  }
+  
+  Profile.findOneAndUpdate({ user: req.user.id, "experience._id": req.params.id }, { '$set': body })
     .then((profile) => {
       res.status(200).send(profile.experience.id(req.params.id));
     });
